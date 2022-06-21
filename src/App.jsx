34c1782a4d5458896d2843f0e9eea7de16599d46
hy <mintlib66@gmail.com>
2022-06-21 20:40:20 +0900

    README 정돈, 주석 정리

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,6 @@ import './style/App.css'
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([])
-  /*
-  useEffect(() => {
-    const reqOption = {
-      method: 'GET',
-      redirect: 'follow',
-    }
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${API_KEY}&part=snippet,contentDetails,statistics,status`
-    )
-      .then(res => res.json())
-      .then(result => result.items[0])
-      .then(item => {
-        console.log(item.snippet.localized.title)
-        console.log(item.snippet.channelTitle)
-        console.log(item.snippet.publishedAt)
-        console.log(item.statistics)
-        setVideos({
-          title: item.snippet.localized.title,
-          channelTitle: item.snippet.channelTitle,
-          publishedAt: item.snippet.publishedAt,
-        })
-      })
-      .catch(err => console.error(err))
-  }, [])*/
 
   const search = useCallback(
     query => {
@@ -46,7 +21,7 @@ function App({ youtube }) {
   return (
     <div className="App">
       <Header onSearch={search}></Header>
-      {/* 메인화면은 포스트 없이 리스트 2줄로 */}
+      {/* 메인화면은 포스트 없이 리스트로 */}
       <div className="main">
         {/* <Post video={videos}></Post> */}
         <VideoList videos={videos}></VideoList>
